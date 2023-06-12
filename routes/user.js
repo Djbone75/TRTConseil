c4ffced7746486dcdaa8062429060a0f9ba1d064
Djbone75 <Djbone94@hotmail.com>
2023-06-12 14:44:42 +0200

    commit 1.5

--- routes/user.js
+++ routes/user.js
@@ -12,7 +12,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, "./uploads"));
+    cb(null, "./uploads/");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname.toLowerCase().split(" ").join("-"));
