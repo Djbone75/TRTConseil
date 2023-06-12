3630270066c147d68e8396bee99b916616c0f806
Djbone75 <Djbone94@hotmail.com>
2023-06-12 08:28:04 +0200

    commit 1.2

--- dbconfig.js
+++ dbconfig.js
@@ -1,19 +1,8 @@
 const pg = require("pg");
 
 const connectionString = process.env.CONNECTION_STRING;
-const pool = new pg.Pool(
-  {
-    connectionString,
-  },
-  (err, result) => {
-    if (err) {
-      return console.log(
-        "problème base de donnée, reverifiez vos identifiants "
-      );
-    }
-
-    next();
-  }
-);
+const pool = new pg.Pool({
+  connectionString,
+});
 
 module.exports = { pool };
