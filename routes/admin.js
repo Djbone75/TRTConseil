c436db42d7075a2289fa288cce339ae3812f124d
Djbone75 <Djbone94@hotmail.com>
2023-06-12 10:43:30 +0200

    commit 1.4

--- routes/admin.js
+++ routes/admin.js
@@ -4,14 +4,11 @@ const adminController = require("../controllers/admin");
 const { checkAdmin, checkConsultant } = require("../middleware");
 
 const router = express.Router();
-//approuver l'enregistrement
 
-router.get("/getUser", checkAdmin, adminController.getUser);
+router.get("/getUser", checkConsultant, adminController.getUser);
 
-//approuver les consultants
 router.get("/getConsultant", checkAdmin, adminController.getConsultant);
 
-//approuver les posts
 router.get("/getPosts", checkConsultant, adminController.getPosts);
 router.post(
   "/approveUser/:userId",
